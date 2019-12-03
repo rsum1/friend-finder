037512ed8cc656dc0bf14909f5c2d46cf26bc30b
rsum1 <ryanjsumpter+github@gmail.com>
2019-12-03 12:48:00 -0800

    routes working, started frontend

--- FriendFinder/app/routing/apiRoutes.js
+++ FriendFinder/app/routing/apiRoutes.js
@@ -1,4 +1,5 @@
-const { friends, surveyQuestions } = require('../data')
+const friends = require('../data/friends')
+const surveyQuestions = require('../data/surveyQuestions')
 
 module.exports = (app) => {
   app.get('/api/friends', (req, res) => {
@@ -6,12 +7,14 @@ module.exports = (app) => {
   })
   
   // A GET route for getting the questions
-  app.get(`/api/questions`, (req, res) => {
+  app.get('/api/questions', (req, res) => {
     res.json(surveyQuestions)
   })
 
   // A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
-  app.post(`/api/friends`, (req, res) => {
+  app.post('/api/friends', (req, res) => {
     console.log(req.body)
+    friends.push(req.body)
+    // res.json(req.body)
   })
 }
